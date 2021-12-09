75249e5ef061fe003ab4d1e1bd63aef329f28dfd
LeonardoGuerrini <90059757+LeonardoGuerrini@users.noreply.github.com>
2021-12-09 20:03:11 -0300

    Update calculadora.py

--- calculadora.py
+++ calculadora.py
@@ -1,55 +1,68 @@
-print("---------------------")
-print("Calculadora em Python")
-print("---------------------")
+import time # Biblioteca time
 
+print('---------------------')
+print('Calculadora em Python')
+print('---------------------\n')
+
+# Criando as funções de soma, subtração, etc.
 
 def soma(x, y):
     return x + y
 
-
 def subtracao(x, y):
     return x - y
 
-
 def multiplicacao(x, y):
     return x * y
-
 
 def divisao(x, y):
     return x / y
 
 
 while True:
-    print('''
+    try:
+        escolha = input('''
+Selecione:
+
 [1] Soma
 [2] Subtração
 [3] Multiplicação
 [4] Divisão
-    ''')
 
-    escolha = input("Sua escolha (1/2/3/4): ")
+Sua escolha: ''')
 
-    print("")
+        if escolha in ('1', '2', '3', '4'):
 
-    if escolha in ("1", "2", "3", "4"):
-        x = float(input("Primeiro número: "))
-        y = float(input("Segundo número: "))
+            x = int(input('Insira um número: '))
+            y = int(input('Insira outro número: '))
 
-        print("")
+            print('') # Pulando uma linha
+            print('') # Pulando uma linha
 
-        if escolha == "1":
-            print(x, "+", y, "=", soma(x, y))
-        elif escolha == "2":
-            print(x, "-", y, "=", subtracao(x, y))
-        elif escolha == "3":
-            print(x, "*", y, "=", multiplicacao(x, y))
-        elif escolha == "4":
-            print(x, "/", y, "=", divisao(x, y))
+            if escolha == '1':
+                print(f'{x} + {y} =', soma(x, y))
+            
+            elif escolha == '2':
+                print(f'{x} - {y} =', subtracao(x, y))
 
-        proxima_operacao = input("\nDeseja fazer outra operação? (sim/nao): ")
-        if proxima_operacao.lower() == "nao":
-            print("Obrigado por usar a calculadora! :)")
-            break
+            elif escolha == '3':
+                print(f'{x} * {y} =', multiplicacao(x, y))
+            
+            elif escolha == '4':
+                print(f'{x} / {y} =', divisao(x, y))
+            
+            # Repetir
 
-    else:
-        print("Escolha inválida.")
+            if str(input('\nVocê deseja repetir? (sim/nao): ')).lower() in ['n', 'nao', 'não']:
+                print('\nObrigado por usar o programa :)')
+                break
+            
+        else:
+            print('\n-------------------')
+            print('Escolha inválida!')
+            print('-------------------')
+            
+            time.sleep(1)
+
+    except:
+        print('\nErro!')
